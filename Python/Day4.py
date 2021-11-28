9b11816c82f0de977822b1ef61af56f79e3ded84
JWaters02 <watersjoshua2002@gmail.com>
2021-11-28 01:37:19 +0000

    Python Setup

---
+++ Python/Day4.py
@@ -0,0 +1,16 @@
+import time
+import fileinput
+
+def main():
+    lines = [line for line in fileinput.input("./Input/Day4.txt")]
+
+def part1():
+    t = 1
+
+def part2():
+    t = 1
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
