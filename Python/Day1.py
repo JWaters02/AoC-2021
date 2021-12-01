c4e680e895094dfbf413ff27ab5219a8667e0eaf
JWaters02 <watersjoshua2002@gmail.com>
2021-12-01 07:12:32 +0000

    Day 1
    Execution time 0.002s

--- Python/Day1.py
+++ Python/Day1.py
@@ -3,12 +3,23 @@ import fileinput
 
 def main():
     lines = [line for line in fileinput.input("./Input/Day1.txt")]
+    nums = [int(line.strip()) for line in lines]
+    print(f"Part 1: {part1(nums)}") 
+    print(f"Part 2: {part2(nums)}")
 
-def part1():
-    t = 1
+def part1(nums):
+    count = 0
+    for i, num in enumerate(nums):
+        if num > nums[i - 1]:
+            count += 1
+    return count
 
-def part2():
-    t = 1
+def part2(nums):
+    count = 0
+    for i, num in enumerate(nums):
+        if num > nums[i - 3]:
+            count += 1
+    return count
 
 if __name__ == "__main__":
     start_time = time.time()
