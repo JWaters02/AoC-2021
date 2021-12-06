fe874d24ad275ab6cd85b068547010890a759baf
JWaters02 <watersjoshua2002@gmail.com>
2021-12-06 05:39:14 +0000

    Day 6

--- Python/Day6.py
+++ Python/Day6.py
@@ -1,16 +1,39 @@
 import time
 import fileinput
+from collections import defaultdict
 
-def part1(lines):
-    pass
+def part1(starting_ages):
+    out = []
+    for i in starting_ages:
+        if i > 0:
+            out.append(i - 1)
+        else:
+            out.extend([6, 8])
+    return out
 
-def part2(lines):
-    pass
+def part2(starting_ages):
+    age_count = defaultdict(int)
+    for fish in starting_ages:
+        age_count[fish] += 1
+
+    for _ in range(256):
+        n = defaultdict(int)
+        for age, count in age_count.items():
+            if age > 0:
+                n[age - 1] += count
+            else:
+                n[6] += count
+                n[8] += count
+        age_count = n
+    return sum(age_count.values())
 
 def main():
     lines = [line for line in fileinput.input("./Input/Day6.txt")]
-    part1(lines)
-    part2(lines)
+    starting_ages = list(map(int, lines[0].split(",")))
+    for _ in range(80):
+        starting_ages = part1(starting_ages)
+    print(len(starting_ages))
+    print(part2(starting_ages))
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
