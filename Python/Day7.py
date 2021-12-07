29848d7d05fa15ca159dd6a4616ec53c45053cde
JWaters02 <watersjoshua2002@gmail.com>
2021-12-07 08:00:52 +0000

    Day 7

--- Python/Day7.py
+++ Python/Day7.py
@@ -1,16 +1,28 @@
 import time
 import fileinput
 
-def part1(lines):
-    pass
+def part1(pos):
+    middle = pos[len(pos) // 2]
+    fuel_usage = sum(abs(x - middle) for x in pos)
+    print(fuel_usage)
 
-def part2(lines):
-    pass
+def part2(pos):
+    def fuel_cost(move):
+        return move * (move + 1) // 2
+    
+    middle = pos[len(pos) // 2] 
+    fuel_usage = sum(fuel_cost(abs(x - middle)) for x in pos)
+
+    for middle in range(min(pos), max(pos) + 1):
+        fuel_usage = min(fuel_usage, sum(fuel_cost(abs(x - middle)) for x in pos))
+    print(fuel_usage)
 
 def main():
     lines = [line for line in fileinput.input("./Input/Day7.txt")]
-    part1(lines)
-    part2(lines)
+    pos = list(map(int, lines[0].split(",")))
+    pos.sort()
+    part1(pos)
+    part2(pos)
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
